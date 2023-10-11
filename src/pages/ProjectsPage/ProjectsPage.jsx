43388c5cdbd3ff67a537a85f2a40bd2604b67f18
Kristin Stockley <kbstockley@gmail.com>
2023-10-11 14:02:44 -0500

    updated project cards

--- src/pages/ProjectsPage/ProjectsPage.jsx
+++ src/pages/ProjectsPage/ProjectsPage.jsx
@@ -1,55 +1,51 @@
 import Card from 'react-bootstrap/Card';
-import './ProjectsPage.scss'
+import './ProjectsPage.scss';
+import { BiLogoDjango, BiLogoReact, BiLogoPython, BiLogoMongodb, BiLogoNodejs } from 'react-icons/bi';
+import { SiExpress } from 'react-icons/si';
+
 
 export default function ProjectsPage() {
 
     return (
         <>
-            <Card className="bg-dark text-white refurred-card mb-2">
+        <h1 className="text-center mt-5">Current Project</h1>
+
+            <Card className="bg-dark text-white refurred-card mb-5">
                 <a className="portfolio-item project-card" href="https://www.re-furred.com/" target="_blank" rel="noreferrer">
                     <div className="card-overlay">
-                        <Card.Img src="assets/images/dogballoons.jpg" alt="refurred Project" className='refurred-img' />
+                        <Card.Img src="assets/images/refurred.png" alt="refurred Project" className='refurred-img' />
                         <Card.ImgOverlay className='text-white' >
-                            <Card.Title>re<span className='furr'>furr</span>ed</Card.Title>
-                            <Card.Text>
-                                Dog breed selector, finder, and directory
-                            </Card.Text>
-                            <Card.Text>MERN | WIP with two fellow GA grads</Card.Text>
+                        <Card.Title className='project-icons d-flex justify-content-end'><BiLogoMongodb /><SiExpress /> <BiLogoReact /> <BiLogoNodejs /></Card.Title>
                         </Card.ImgOverlay>
                     </div>
                 </a>
             </Card>
+            
+            <hr />
 
+            <h1 className="text-center mt-5">Class Projects</h1>
             <Card className="bg-dark text-white cmi-card mb-2">
                 <a className="portfolio-item project-card" href="https://count-me-in.herokuapp.com/" target="_blank" rel="noreferrer">
                     <div className="card-overlay">
-                        <Card.Img src="assets/images/party.jpg" alt="CMI Project" className='cmi-img' />
+                        <Card.Img src="assets/images/count-me-in.png" alt="CMI Project" className='cmi-img' />
                         <Card.ImgOverlay className='text-white' >
-                            <Card.Title>CountMeIn</Card.Title>
-                            <Card.Text>
-                                Track upcoming occasions with personal countdowns
-                            </Card.Text>
-                            <Card.Text>MERN | Class Project</Card.Text>
+                            <Card.Title className='project-icons d-flex justify-content-end mb-5'><BiLogoMongodb /><SiExpress /> <BiLogoReact /> <BiLogoNodejs /></Card.Title>
                         </Card.ImgOverlay>
                     </div>
                 </a>
             </Card>
 
-            <Card className="bg-dark text-white swiftseats-card">
+            <Card className="bg-dark text-white swiftseats-card mb-5">
                 <a className="portfolio-item project-card" href="https://swiftseats.herokuapp.com/" target="_blank" rel="noreferrer">
                     <div className="card-overlay">
-                        <Card.Img src="assets/images/confetti-portfolio.jpg" alt="SwiftSeats Project" className='swiftseats-img' />
+                        <Card.Img src="assets/images/swift-seats.png" alt="SwiftSeats Project" className='swiftseats-img' />
                         <Card.ImgOverlay className='text-white' >
-                            <Card.Title>SwiftSeats</Card.Title>
-                            <Card.Text>
-                                Concert tracker and photo storage for Swifties
-                            </Card.Text>
-                            <Card.Text>Python | Django | Class Project</Card.Text>
+                            <Card.Title className='project-icons d-flex justify-content-end mb-6'><BiLogoPython /> <BiLogoDjango /></Card.Title>
                         </Card.ImgOverlay>
                     </div>
                 </a>
-            </Card>
 
-        </>
+            </Card>
+                    </>
     )
 }
